c69a6e0552590c794cfafe74e7685fab66ce3899
Leonardo <leofronchi@outlook.com>
2026-03-10 17:23:42 -0300

    adicionando cookie

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,25 +1,24 @@
-const {signUpService, signInService} = require('@services/userService')
+const { signUpService, signInService } = require("@services/userService");
 
 const signUp = async (req, res, next) => {
   try {
-    const {username, email, password} = req.body;
-    const newUser = await signUpService({username, email, password});
+    const { username, email, password } = req.body;
+    await signUpService({ username, email, password });
 
-    res.status(201).json({ user: newUser });
+    res.status(201).json({ message: "User created successfully" });
   } catch (error) {
     next(error);
   }
 };
 
-const signIn = async (req, res, next) =>
-{
+const signIn = async (req, res, next) => {
   try {
-    const {email, password} = req.body;
-    const user = await signInService({email, password});
+    const { email, password } = req.body;
+    const user = await signInService({ email, password });
     res.status(200).json({ user: user });
   } catch (error) {
     next(error);
   }
-}
+};
 
-module.exports = {signUp, signIn}
+module.exports = { signUp, signIn };
--- services/userService.js
+++ services/userService.js
@@ -1,58 +1,63 @@
 const { User } = require("../models");
 const { jwtSign } = require("../helpers/jwt");
-const {bcryptHash, bcryptCompare} = require("../helpers/bcrypt");
-const {FieldRequiredError, AlreadyTakenError} = require("../helpers/customErrors")
+const { bcryptHash, bcryptCompare } = require("../helpers/bcrypt");
+const {
+  FieldRequiredError,
+  AlreadyTakenError,
+} = require("../helpers/customErrors");
 
-async function signUpService({username, email, password}) {
-    if (!username) throw new FieldRequiredError(`A username`);
-    if (!email) throw new FieldRequiredError(`An email`);
-    if (!password) throw new FieldRequiredError(`A password`);
-
-     const userExists = await User.findOne({
-      where: { email: email },
-    });
-    if (userExists) throw new AlreadyTakenError("Email", "try logging in");
-    const newUser = await User.create({
-      email: email,
-      username: username,
-      password: await bcryptHash(password),
-    });
-    newUser.dataValues.token = await jwtSign({
-    id: newUser?.id,
-    email: newUser?.email,
-    })
-    return {
-        id: newUser.id,
-        email: newUser.email,
-        username: newUser.username,
-        token: newUser.dataValues.token
-    }
-}
-
-async function signInService({email, password}) {
+async function signUpService({ username, email, password }) {
+  if (!username) throw new FieldRequiredError(`A username`);
   if (!email) throw new FieldRequiredError(`An email`);
   if (!password) throw new FieldRequiredError(`A password`);
 
-  const user = await User.findOne({ where: {email: email}})
+  const userExists = await User.findOne({
+    where: { email: email },
+  });
+  if (userExists) throw new AlreadyTakenError("Email", "try logging in");
+  const newUser = await User.create({
+    email: email,
+    username: username,
+    password: await bcryptHash(password),
+  });
+  newUser.dataValues.token = await jwtSign({
+    id: newUser?.id,
+    email: newUser?.email,
+  });
+  return {
+    id: newUser.id,
+    email: newUser.email,
+    username: newUser.username,
+    token: newUser.dataValues.token,
+  };
+}
+
+async function signInService({ email, password }) {
+  if (!email) throw new FieldRequiredError(`An email`);
+  if (!password) throw new FieldRequiredError(`A password`);
+
+  const user = await User.findOne({ where: { email: email } });
 
   if (!user) {
-    throw new Error('Invalid credentials');
+    throw new Error("Invalid credentials");
   }
 
   if (!(await bcryptCompare(password, user.password))) {
-    throw new Error('Invalid credentials');
-}
-  user.dataValues.token = await jwtSign({
-  id: user?.id,
-  email: user?.email,
-});
+    throw new Error("Invalid credentials");
+  }
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "Strict",
+    maxAge: 24 * 60 * 60 * 1000,
+  });
 
   return {
     id: user?.id,
     email: user?.email,
     username: user?.username,
-    token: user?.dataValues.token
-  }
+  };
 }
 
-module.exports = {signUpService, signInService}
+module.exports = { signUpService, signInService };
